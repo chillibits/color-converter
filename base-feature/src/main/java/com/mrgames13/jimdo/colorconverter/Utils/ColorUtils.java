5bfefc98d8a22b1693cf083cb3f62b54626fd4c3
M&R Games <mrgames@outlook.de>
2019-04-07 20:15:59 +0200

    Fixed bug on image selection

---
+++ base-feature/src/main/java/com/mrgames13/jimdo/colorconverter/Utils/ColorUtils.java
@@ -0,0 +1,65 @@
+package com.mrgames13.jimdo.colorconverter.Utils;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.support.v7.graphics.Palette;
+
+import com.mrgames13.jimdo.colorconverter.R;
+
+public class ColorUtils {
+    //Konstanten
+
+    //Variablen als Objekte
+    private Resources res;
+
+    //Variablen
+
+    public ColorUtils(Resources res) {
+        this.res = res;
+    }
+
+    public int getVibrantColor(Bitmap image) {
+        Palette palette = Palette.from(image).generate();
+        return palette.getVibrantColor(res.getColor(R.color.grey));
+    }
+
+    public int getLightVibrantColor(Bitmap image) {
+        Palette palette = Palette.from(image).generate();
+        return palette.getLightVibrantColor(res.getColor(R.color.grey));
+    }
+
+    public int getDarkVibrantColor(Bitmap image) {
+        Palette palette = Palette.from(image).generate();
+        return palette.getDarkVibrantColor(res.getColor(R.color.grey));
+    }
+
+    public int getMutedColor(Bitmap image) {
+        Palette palette = Palette.from(image).generate();
+        return palette.getMutedColor(res.getColor(R.color.grey));
+    }
+
+    public int getLightMutedColor(Bitmap image) {
+        Palette palette = Palette.from(image).generate();
+        return palette.getLightMutedColor(res.getColor(R.color.grey));
+    }
+
+    public int getDarkMutedColor(Bitmap image) {
+        Palette palette = Palette.from(image).generate();
+        return palette.getDarkMutedColor(res.getColor(R.color.grey));
+    }
+
+    public int darkenColor(int color) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(color, hsv);
+        hsv[2] *= 0.8f;
+        return Color.HSVToColor(hsv);
+    }
+
+    public int getTextColor(int color) {
+        int sum = Color.red(color) + Color.green(color) + Color.blue(color);
+        if(sum > 384) return Color.BLACK;
+        return Color.WHITE;
+    }
+
+}
