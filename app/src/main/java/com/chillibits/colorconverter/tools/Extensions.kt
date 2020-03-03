e704797e0277d71ff72070cb44a2e7e3e2d87910
Marc Auberer <marc.auberer@chillibits.com>
2020-03-03 22:29:28 +0100

    Prepare for release of version 2.1.1

--- app/src/main/java/com/chillibits/colorconverter/tools/Extensions.kt
+++ app/src/main/java/com/chillibits/colorconverter/tools/Extensions.kt
@@ -4,7 +4,6 @@
 
 package com.chillibits.colorconverter.tools
 
-import android.content.Context
 import android.content.res.ColorStateList
 import android.content.res.Resources
 import android.widget.ImageView
@@ -14,4 +13,4 @@ fun ImageView.setTint(color: Int) {
     ImageViewCompat.setImageTintList(this, ColorStateList.valueOf(color))
 }
 
-fun Context.dpToPx(dp: Int) = (dp * Resources.getSystem().displayMetrics.density).toInt()
+fun dpToPx(dp: Int) = (dp * Resources.getSystem().displayMetrics.density).toInt()
